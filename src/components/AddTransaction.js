b3e8163ec502b0a4ba2cb742e3f3d08048d54123
Rivaldo Esbend <esbendrivaldo@gmail.com>
2022-07-02 14:09:39 +0200

    expense tracker done

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -6,12 +6,24 @@ export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
 
-    const { deleteTransaction }=  useContext(GlobalContext);
+    const { addTransaction }=  useContext(GlobalContext);
+
+    const onSubmit = e => {
+      e.preventDefault();
+
+      const newTransaction = {
+        id: Math.floor(Math.random() * 100000000),
+        text,
+        amount: +amount
+      }
+
+      addTransaction(newTransaction);
+    }
 
   return (
     <>
         <h3>Add new transaction</h3>
-        <form id="form">
+        <form onSubmit={onSubmit}>
             <div className="form-control">
             <label htmlFor="text">Text</label>
             <input type="text" value={text} onChange={(e) => setText(e.target.value)} id="text" placeholder="Enter text..." />
