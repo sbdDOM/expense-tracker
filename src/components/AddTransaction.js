9f9a1e3e5f2596bb6f55f00ea2693cb26cf34eb1
Rivaldo Esbend <esbendrivaldo@gmail.com>
2022-07-02 12:10:36 +0200

    Delete, +-, adding transaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,8 +1,12 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
+
+    const { deleteTransaction }=  useContext(GlobalContext);
 
   return (
     <>
